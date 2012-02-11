022d95f3cbc09c6c94c379568cd60d8c640a7852
marimofire <marimofire@gmail.com>
2012-02-11 13:34:26 +0900

    first commit

---
+++ c4saconfig.php
@@ -0,0 +1,7 @@
+<?php
+define("C4SAAPPNAME", "presidentapp");
+define("C4SAMYSQL", "mysqlserver");
+define("C4SADB", "presidentapp");
+define("C4SADBUSER", "presidentapp");
+define("C4SADBPASSWD", "61190552c97");
+?>
